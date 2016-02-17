205c5e9ad54e5fd7e2cd911d3a00b1e64a0522c7
Nitin Anand <nitinnatural@gmail.com>
2016-02-18 03:47:08 +0530

    project setup

---
+++ app/src/main/java/com/androidyug/mindbody/MindBodyApp.java
@@ -0,0 +1,15 @@
+package com.androidyug.mindbody;
+
+import android.app.Application;
+
+/**
+ * Created by IAMONE on 2/18/2016.
+ */
+public class MindBodyApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
